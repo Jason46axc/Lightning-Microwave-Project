6848b28b492e903c5b7ac55ede988657ea9394a3
Jason46axc <Jason46axc@gmail.com>
2016-08-06 08:53:51 +0800

    Add files via upload

--- lib/usrp_source_impl.cc
+++ lib/usrp_source_impl.cc
@@ -88,7 +88,7 @@ namespace gr {
 		Counter = 0;// 计数器清零
 		TriPos = 0;// 标记清零
 		TriFlag = false;// 重置标志
-		// TriThreshold = 0.000001;
+		// TriThreshold = 0.001;
       
 #ifdef GR_UHD_USE_STREAM_API
       _samps_per_packet = 1;
@@ -385,6 +385,7 @@ namespace gr {
 	{
 		TriThreshold = threshold;
 		// double aaataaaa = threshold;
+		// aaataaaa++;
 	}
 
     void
